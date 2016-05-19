6114ea1d1516fb72dc358cdf72612e3e26cd6a32
PatrickCooke <cookepa1@gmail.com>
2016-05-19 12:30:06 -0400

    Initial Commit

---
+++ VineLog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VineLog
+//
+//  Created by Patrick Cooke on 5/19/16.
+//  Copyright © 2016 Patrick Cooke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
